40bcab9129f2c2eaec2b29402cadaa015a844277
John Pettigrew <john.pettigrew404@gmail.com>
2014-09-14 01:32:31 -0500

    Change event iframe width.

--- script/script.js
+++ script/script.js
@@ -1,18 +1,15 @@
-
-
 $(document).ready(function(){
 	//Decide which view is needed.
 	var desktopContent = "<video class=\"main-video\" width=\"100%\" autoplay loop><source src=\"video/southern rift video.mp4\" type=\"video/mp4\"><img src=\"images/picture2.png\" id=\"cover\"></video>";
 	var mobileContent = "<img src=\"images/picture2.png\" id=\"cover\">";
 	if(screen.width < 800){
 		//set iframe size
-		$('.event-frame').css('width', screen.width);
 		$('#main-page-content').html(mobileContent);
 	}
 	else{
-		$('.event-frame').css('width', screen.width/2);
 		$('#main-page-content').html(desktopContent);
 	}
+	//$('.event-frame').css('width', screen.width/2);
 	$('.event-frame').css('height', screen.height);
 	//animations.
 	$('.list_item').hover(function(){
